c558cb182a2501931300dbd707220b06ebb5bd71
Priya <priya.beniwal@housimg.com>
2025-01-30 12:04:55 +0530

    Add comment

--- housing/src/main/java/com/example/housing/exception/BookNotBorrowedException.java
+++ housing/src/main/java/com/example/housing/exception/BookNotBorrowedException.java
@@ -1,5 +1,5 @@
 package com.example.housing.exception;
-
+//Book not borrowed exception will throw an error if the member is trying to return a book the member never borrowed
 public class BookNotBorrowedException extends RuntimeException {
     public BookNotBorrowedException(String msg) {
         super(msg);
