81b8385560a5dbf080ad1a045902cb3fb0313bb3
Priya <priya.beniwal@housimg.com>
2025-02-07 18:12:35 +0530

    Jpa and Validations

--- HousingLibrary/src/main/java/com/eg/HousingLibrary/model/BookLending.java
+++ HousingLibrary/src/main/java/com/eg/HousingLibrary/model/BookLending.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 public class BookLending {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @ManyToOne
